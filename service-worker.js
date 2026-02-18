e06cea6f4ffa86f924b480ff7de2cc992488b2b0
americalara <0322103716@ut-tijuana.edu.mx>
2026-02-18 01:39:59 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "frases-cache-v5"; // cambiar el número cada vez que se actualice algo
+const CACHE_NAME = "frases-cache-v6"; // cambiar el número cada vez que se actualice algo
 
 
 const urlsToCache = [
