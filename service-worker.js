1d4bbace5201864dc4f88b8db685439c92b15e72
americalara <0322103716@ut-tijuana.edu.mx>
2026-02-18 00:29:08 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,49 @@
-const CACHE_NAME = "quotes-app-v4";
+const CACHE_NAME = "frases-cache-v2";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+    "./",
+    "./index.html",
+    "./script.js",
+    "./manifest.json"
 ];
 
+// INSTALAR
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
 });
 
+// ACTIVAR
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// FETCH
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .catch(() => caches.match(event.request))
-  );
+
+    
+    if (event.request.url.includes("api.quotable.io")) {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
 });
